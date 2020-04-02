47ecd1545339342a75404b3f7fdabc0e44f8c2db
parthagarwal <parth.agarwal@treebohotels.com>
2020-04-02 20:38:01 +0530

    Django-environ for configuration.

--- project/settings.py
+++ project/settings.py
@@ -12,19 +12,26 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import environ
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Integrating project with os environment
+env = environ.Env(DEBUG=(bool, False))
+env_file = os.path.join(BASE_DIR,'.env')
+environ.Env.read_env(env_file)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't(exr&*w6u8-ndiqsti4as9w)q2swywoe-36sh$((d$)aaz&g('
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
