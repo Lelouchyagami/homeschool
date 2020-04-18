64520966431daa497f7e7b4ef7b59e4d2b225e61
parthagarwal <parth.agarwal@treebohotels.com>
2020-04-18 23:25:29 +0530

    Added test for day_of_week.

--- homeschool/schools/tests/test_models.py
+++ homeschool/schools/tests/test_models.py
@@ -1,3 +1,4 @@
+from homeschool.schools.models import SchoolYear
 from homeschool.schools.tests.factories import (
     GradeLevelFactory,
     SchoolFactory,
@@ -33,19 +34,43 @@ class TestSchoolYear(TestCase):
 
         self.assertEqual(school_year.school, school)
 
+    def test_has_days_of_week(self):
+        days_of_week = SchoolYear.MONDAY + SchoolYear.TUESDAY
+        school_year = SchoolYearFactory(days_of_week=days_of_week)
+
+        self.assertEqual(school_year.days_of_week, days_of_week)
+
+    def test_runs_on(self):
+        school_year = SchoolYearFactory(days_of_week=SchoolYear.MONDAY)
+
+        self.assertTrue(school_year.runs_on(SchoolYear.MONDAY))
+        self.assertFalse(school_year.runs_on(SchoolYear.TUESDAY))
+
+    def test_days_of_week_default(self):
+        school_year = SchoolYearFactory()
+
+        self.assertTrue(school_year.runs_on(SchoolYear.MONDAY))
+        self.assertTrue(school_year.runs_on(SchoolYear.TUESDAY))
+        self.assertTrue(school_year.runs_on(SchoolYear.WEDNESDAY))
+        self.assertTrue(school_year.runs_on(SchoolYear.THURSDAY))
+        self.assertTrue(school_year.runs_on(SchoolYear.FRIDAY))
+        self.assertFalse(school_year.runs_on(SchoolYear.SATURDAY))
+        self.assertFalse(school_year.runs_on(SchoolYear.SUNDAY))
+
 
 class TestGradeLevel(TestCase):
     def test_factory(self):
         grade_level = GradeLevelFactory()
 
         self.assertIsNotNone(grade_level)
+        self.assertNotEqual(grade_level.name, "")
 
     def test_has_name(self):
         name = "Kindergarten"
         grade_level = GradeLevelFactory(name=name)
 
         self.assertEqual(grade_level.name, name)
-        
+
     def test_has_school_year(self):
         school_year = SchoolYearFactory()
         grade_level = GradeLevelFactory(school_year=school_year)
