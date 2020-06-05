96946a7ac805e4846027a8531aa60e47799597da
parthagarwal <parth.agarwal@treebohotels.com>
2020-06-05 21:04:55 +0530

    Get closer to the real schedule to display for AppView.

--- homeschool/students/models.py
+++ homeschool/students/models.py
@@ -1,9 +1,13 @@
+
 from django.db import models
 
-# Create your models here.
+
 class Student(models.Model):
     """The learner"""
-    school = models.ForeignKey("schools.School",on_delete=models.CASCADE)
+
+    school = models.ForeignKey(
+        "schools.School", on_delete=models.CASCADE, related_name="students"
+    )
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
 
@@ -14,15 +18,33 @@ class Student(models.Model):
     def __str__(self):
         return self.full_name
 
+    def get_courses(self, school_year):
+        """Get the courses from the school year."""
+        enrollment = Enrollment.objects.filter(
+            student=self, grade_level__in=school_year.grade_levels.all()
+        ).first()
+        if enrollment:
+            # This looks goofy, but it operates under the assumption
+            # school year did all the prefetching on grade levels and courses.
+            for grade_level in school_year.grade_levels.all():
+                if grade_level.id == enrollment.grade_level_id:
+                    return list(grade_level.courses.all())
+        return []
+
+
 class Enrollment(models.Model):
-    """ The association between a student and grade level """
-    student = models.ForeignKey(Student,on_delete=models.CASCADE)
-    grade_level = models.ForeignKey("schools.GradeLevel", on_delete=models.CASCADE , related_name="students")
+    """The association between a student and grade level"""
+
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    grade_level = models.ForeignKey("schools.GradeLevel", on_delete=models.CASCADE)
+
 
 class Coursework(models.Model):
     """The work that student completes for course tasks"""
+
     class Meta:
         verbose_name_plural = "coursework"
-    student = models.ForeignKey(Student,on_delete=models.CASCADE)
-    course_task = models.ForeignKey("courses.CourseTask",on_delete=models.CASCADE)
+
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course_task = models.ForeignKey("courses.CourseTask", on_delete=models.CASCADE)
     completed_date = models.DateField(db_index=True)
