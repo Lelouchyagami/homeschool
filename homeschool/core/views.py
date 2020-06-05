f0a617b58dadf0c14c110d5bb7e30e26f06a1258
parthagarwal <parth.agarwal@treebohotels.com>
2020-06-05 22:10:17 +0530

    Finish week view.

--- homeschool/core/views.py
+++ homeschool/core/views.py
@@ -1,3 +1,4 @@
+
 from dateutil.relativedelta import MO, SU, relativedelta
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
@@ -16,7 +17,12 @@ class AppView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
+
+        # This is UTC so it is not localized to the user's timezone.
+        # That may lead to funny results in the evening.
         today = timezone.now().date()
+        context["today"] = today
+
         week = self.get_week_boundaries(today)
         context["monday"], context["sunday"] = week
 
@@ -26,7 +32,12 @@ class AppView(LoginRequiredMixin, TemplateView):
             .first()
         )
 
-        context["schedules"] = self.get_schedules(school_year, week)
+        week_dates = []
+        if school_year:
+            week_dates = school_year.get_week_dates_for(week)
+        context["week_dates"] = week_dates
+
+        context["schedules"] = self.get_schedules(school_year, week, week_dates)
         return context
 
     def get_week_boundaries(self, today):
@@ -35,13 +46,12 @@ class AppView(LoginRequiredMixin, TemplateView):
         sunday = today + relativedelta(weekday=SU(+1))
         return monday, sunday
 
-    def get_schedules(self, school_year, week):
+    def get_schedules(self, school_year, week, week_dates):
         """Get the schedules for each student."""
         schedules = []
         if school_year is None:
             return schedules
 
-        week_dates = school_year.get_week_dates_for(week)
         for student in self.request.user.school.students.all():
             courses = student.get_courses(school_year)
             week_coursework = self.get_student_week_coursework(student, courses, week)
@@ -90,26 +100,22 @@ class AppView(LoginRequiredMixin, TemplateView):
             # If it's possible to do a single query of all tasks
             # that groups by course then that would be better.
             # No need to over-optimize until that's a real issue.
+            # I brought this up on the forum. It doesn't look like it's easy to fix.
+            # https://forum.djangoproject.com/t/grouping-by-foreignkey-with-a-limit-per-group/979
             course_tasks = list(
                 course.course_tasks.exclude(id__in=completed_task_ids)[:task_limit]
             )
             course_tasks.reverse()
             for week_date in week_dates:
+                course_schedule_item = {"week_date": week_date}
                 if (
                     course.id in week_coursework
                     and week_date in week_coursework[course.id]
                 ):
                     coursework_list = week_coursework[course.id][week_date]
-                    course_schedule["days"].append({"coursework": coursework_list})
-                    continue
-
-                if course.runs_on(week_date):
-                    if course_tasks:
-                        course_schedule["days"].append({"task": course_tasks.pop()})
-                    else:
-                        # There may be no tasks left to pull from.
-                        course_schedule["days"].append(None)
-                else:
-                    course_schedule["days"].append(None)
+                    course_schedule_item["coursework"] = coursework_list
+                elif course.runs_on(week_date) and course_tasks:
+                    course_schedule_item["task"] = course_tasks.pop()
+                course_schedule["days"].append(course_schedule_item)
             schedule["courses"].append(course_schedule)
         return schedule
